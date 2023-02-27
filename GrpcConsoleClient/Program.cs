5a7439c3303e9190f340536432a5d837a4826d09
Altug Celik <altug_81@hotmail.com>
2023-02-27 16:22:35 +0300

    grpc

---
+++ GrpcConsoleClient/Program.cs
@@ -0,0 +1,21 @@
+﻿using Grpc.Net.Client;
+
+class Program
+{
+    static async Task Main(string[] args)
+    {
+        GrpcChannel channel = GrpcChannel.ForAddress("https://localhost:7180");
+        GrpcServer.Greeter.GreeterClient client = new GrpcServer.Greeter.GreeterClient(channel);
+
+        var request = new GrpcServer.HelloRequest
+        {
+            Name = "Altuğ"
+        };
+
+        var response = await client.SayHelloAsync(request);
+
+        Console.WriteLine($"Response from server: {response.Message}");
+
+        Console.ReadLine();
+    }
+}
